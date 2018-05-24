d0a6f0a95f01c957a99ff55b80cfe8c420694108
Birju Vachhani <brvachhani@gmail.com>
2018-05-24 16:48:41 +0530

    Initial commit

---
+++ app/src/main/java/com/birjuvachhani/splashscreendemo/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.birjuvachhani.splashscreendemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        //starting main activity of the app from splash screen activity
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
